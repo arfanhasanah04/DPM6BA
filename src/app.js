e165cb0a641bc6131e625b04a885f9f3bf23ef8b
arfanhasanah04 <arfanhasanah@gmail.com>
2024-12-24 15:39:13 +0700

    first commit

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const authRoutes = require('./routes/authRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 8081;
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.use(cors()); 
 app.use(express.json());
